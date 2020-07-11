bc76f1f4725a15384cfe717e19db006437b43e42
bigwudan <bigwudan@qq.com>
2020-07-11 14:09:01 +0800

    567

--- itu_gui/itu_gui/ite/itu_surface.c
+++ itu_gui/itu_gui/ite/itu_surface.c
@@ -1,6 +1,6 @@
 #include "itu.h"
 #include "itu_cfg.h"
-
+#include <assert.h>
 void ituSurfaceSetClipping(ITUSurface *surf, int x, int y, int w, int h)
 {
     if (x == 0 && y == 0 && w == 0 && h == 0)
@@ -12,4 +12,22 @@ void ituSurfaceSetClipping(ITUSurface *surf, int x, int y, int w, int h)
     surf->clipping.y      = y > 0 ? y : 0;
     surf->clipping.width  = (w <= surf->width) ? w : surf->width;
     surf->clipping.height = (h <= surf->height) ? h : surf->height;
+}
+
+void ituSurfaceRelease(ITUSurface *surf)
+{
+	ITUSurface *parentSurf = (ITUSurface *)surf->parent;
+
+	if (parentSurf && (parentSurf->flags & ITU_STATIC))
+	{
+		assert(parentSurf->flags & ITU_COMPRESSED);
+		assert(parentSurf->lockSize);
+
+		if (--parentSurf->lockSize == 0)
+		{
+			ituDestroySurface(surf);
+		}
+	}
+	else
+		ituDestroySurface(surf);
 }
